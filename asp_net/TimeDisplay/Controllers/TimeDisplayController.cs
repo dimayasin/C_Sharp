55c18636c169fc37728d97d45e9732d274d0c3b4
dimayasin <dyasin@gmail.com>
2018-03-08 17:41:13 -0600

    Time Display

---
+++ asp_net/TimeDisplay/Controllers/TimeDisplayController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace TimeDisplay.Controllers
+{
+    public class TimeDisplayController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
