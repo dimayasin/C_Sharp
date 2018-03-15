ade0e1b4287f050352e34dd9332a48cb16d4c9d2
dimayasin <dyasin@gmail.com>
2018-03-15 11:48:21 -0500

    ajax-Notes and Quoting Dojo

---
+++ asp_net/AjaxNotes/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AjaxNotes.Models;
+using DBConnection;
+
+namespace AjaxNotes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string query = "SELECT * FROM notes";
+            var notes = DBConnector.Query(query);
+            ViewBag.notes = notes;
+            return View();
+        }
+
+         [HttpPost]
+        [Route("/add")]
+        public IActionResult Add(string title, string noteMsg)
+        {
+            string query = $"INSERT INTO notes (title, description, createdAt, updatedAt) VALUES('{title}', '{noteMsg}', NOW(), NOW())";
+            DBConnector.Execute(query);
+            return Redirect("/");
+        }
+
+        [Route("/update/{nid}")]
+        public IActionResult Update(int nid, string title, string noteMsg)
+        {
+            string query=$"UPDATE notes SET title='{title}', description='{noteMsg}', updatedAt=NOW() WHERE id={nid};";
+            DBConnector.Execute(query);
+            return Redirect("/");
+        }
+              [Route("/delete/{nid}")]
+        public IActionResult delete(int nid)
+        {
+            if((nid > 1) && (nid < 3000))
+            {
+                string query=$"DELETE FROM notes WHERE id='{nid}';";
+                DBConnector.Execute(query);
+                return Redirect("/");
+            }
+            else
+            {
+                ViewBag.Error = "invalid entry";
+                return Redirect("/");
+            }
+        }
+    }
+}
---
+++ asp_net/QuotingDojo/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+using DBConnection;
+using System.Globalization;
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBConnector cnx;
+        public HomeController()
+        {
+            cnx = new DBConnector();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        [Route("add")]
+        public IActionResult add(string username, string quote)
+        {
+            string myquery;
+ 
+            
+            if (ModelState.IsValid)
+            {
+
+                myquery = $"INSERT INTO quotes (UserName,Quote,createdAt) Values ('{username}','{quote}',NOW())"; 
+
+                DBConnector.Execute(myquery);
+                return RedirectToAction("show");
+            }
+            // return RedirectToAction("DisplayData");
+            else
+            {
+                ViewBag.errors = ModelState.Values;
+                ViewBag.status = "fail";
+
+                return View("home");
+            }
+
+        }
+        [HttpGet]
+        [Route("show")]
+        public IActionResult show()
+        {
+            List<Dictionary<string, object>> allQuotes = DBConnector.Query("Select * From quotes;");
+
+            // Console.WriteLine("==============================");
+            // Console.WriteLine(allQuotes.Count);
+
+            // Console.WriteLine("==============================");
+
+            ViewBag.allQuotes = allQuotes;
+            return View("home");
+
+        }
+
+    }
+}
